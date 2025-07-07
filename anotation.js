40836d58f75ff07e7b7ea99aeddfb3486c4881c2
Ermeson Ramos de Lima <ermesonramos1234@gmail.com>
2025-07-07 15:03:09 -0300

    Visão Geral | Aprendendo Orientação a Obejtos em TS

---
+++ anotation.js
@@ -0,0 +1,38 @@
+/*
+    ! POO com TypeSript - Programação orientada a objetos
+
+    * 4 pilares do POO
+        ? 1. Abstração: 
+            -> Transforma o nosso problema do mundo real mais próximo possivel para ánalise e desenvolvimento na programção.
+
+        ? 2. Encapsulamento:
+            -> Encapsulamento é o princípio de proteger os dados de um objeto, controlando como eles são acessados e modificados.
+
+        ? 3. Herança:
+            -> Herança é o conceito que permite uma classe "filha" herdar os atributos e métodos de uma classe "pai".
+
+        ? 4. Polimorfismo:
+            -> Em POO, é a capacidade de um método se comportar de maneiras diferentes dependendo do contexto, geralmente usando herança.
+
+    * Estrutura
+        ? 1. Classe:
+            -> Abstração do que há em comum(em termos de características e comportamentos) a um conjunto de objetos.
+            ? 1.1. Atributos:
+                -> Características.
+            ? 2.1. Métodos:
+                -> Comportamentos.
+
+        ? 2. Objeto:
+            -> Combina a estrutura de dados(atributos) e o comportamento dos dados(operações/métodos) em uma única entidade.
+
+            -> Um objeto pode ser definido como um conceito, uma abstração, algo com limites nítidos e significado em relação ao mundo.
+
+            -> Onjetos servem como uma base real para a elaboração de um sistema consistente com uma realidade modelada.
+
+        ? 3. Classe e Objetos
+            1. Classe é o que projetamos.
+
+            2. Objeto é o que criamos a partir da classe.
+
+            3. As classes estão para os objetos assim como as plantas arquitetônicas estão para as casas.
+*/
--- dist/poo-typescript.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-function sum(num1, num2) {
-    console.log(num1 + num2);
-}
-sum(1, 2);
